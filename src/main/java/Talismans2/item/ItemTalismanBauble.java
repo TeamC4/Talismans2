3badd1470106b3d6a0c7cd94286eb79681b27a20
Gig <thegigabit101@gmail.com>
2014-09-09 18:29:10 +0100

    Added Right clicking Talismans Equips Them

--- src/main/java/Talismans2/item/ItemTalismanBauble.java
+++ src/main/java/Talismans2/item/ItemTalismanBauble.java
@@ -1,11 +1,15 @@
 package Talismans2.item;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import Talismans2.Talismans2;
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
+import baubles.common.container.InventoryBaubles;
+import baubles.common.lib.PlayerHandler;
 
 /**
  * @author Gigabit101
@@ -36,21 +40,49 @@ public class ItemTalismanBauble extends Item implements IBauble{
 	}
 
 	@Override
-	public void onEquipped(ItemStack arg0, EntityLivingBase arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onEquipped(ItemStack arg0, EntityLivingBase arg1) {}
 
 	@Override
-	public void onUnequipped(ItemStack arg0, EntityLivingBase arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void onUnequipped(ItemStack arg0, EntityLivingBase arg1) {}
 
 	@Override
-	public void onWornTick(ItemStack arg0, EntityLivingBase arg1) {
-		// TODO Auto-generated method stub
-		
+	public void onWornTick(ItemStack arg0, EntityLivingBase arg1) {}
+	
+	@Override
+	//Equips Talismans When Right Clicked In Players Hand
+	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,
+			EntityPlayer par3EntityPlayer) {
+		InventoryBaubles baubles = PlayerHandler
+				.getPlayerBaubles(par3EntityPlayer);
+		for (int i = 0; i < baubles.getSizeInventory(); i++) {
+			if (baubles.isItemValidForSlot(i, par1ItemStack)) {
+				ItemStack stackInSlot = baubles.getStackInSlot(i);
+				if (stackInSlot == null
+						|| ((IBauble) stackInSlot.getItem()).canUnequip(
+								stackInSlot, par3EntityPlayer)) {
+					if (!par2World.isRemote) {
+						baubles.setInventorySlotContents(i,
+								par1ItemStack.copy());
+						if (!par3EntityPlayer.capabilities.isCreativeMode)
+							par3EntityPlayer.inventory
+									.setInventorySlotContents(
+											par3EntityPlayer.inventory.currentItem,
+											null);
+					}
+
+					onEquipped(par1ItemStack, par3EntityPlayer);
+
+					if (stackInSlot != null) {
+						((IBauble) stackInSlot.getItem()).onUnequipped(
+								stackInSlot, par3EntityPlayer);
+						return stackInSlot.copy();
+					}
+					break;
+				}
+			}
+		}
+
+		return par1ItemStack;
 	}
 
 }
