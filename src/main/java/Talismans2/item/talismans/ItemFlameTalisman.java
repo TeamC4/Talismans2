945a51c007ff5da28e84d183ab6ef2c674d689c1
Gig <thegigabit101@gmail.com>
2014-09-09 18:29:39 +0100

    Added Flame Talisman now returns Fire resistance while equiped

--- src/main/java/Talismans2/item/talismans/ItemFlameTalisman.java
+++ src/main/java/Talismans2/item/talismans/ItemFlameTalisman.java
@@ -1,6 +1,10 @@
 package Talismans2.item.talismans;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import Talismans2.item.ItemTalismanBauble;
 
 /**
@@ -16,6 +20,18 @@ public class ItemFlameTalisman extends ItemTalismanBauble{
 	@Override
 	public void registerIcons(IIconRegister iconRegister) {
 		itemIcon = iconRegister.registerIcon("talismans2:Flame");
+	}
+
+	@Override
+	public void onWornTick(ItemStack itemstack, EntityLivingBase player) {
+		if (!player.isPotionActive(Potion.fireResistance)) {
+			player.addPotionEffect(new PotionEffect(Potion.fireResistance.id,
+					Integer.MAX_VALUE, 1, true));
+		}
+	}
+	// Removes Potion effect on Unequip
+	public void onUnequipped(ItemStack itemstack, EntityLivingBase player) {
+			player.removePotionEffect(Potion.fireResistance.id);
 	}
 
 }
