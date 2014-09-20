3e15364599c22250eb089f4312366594f53536e3
Gig <thegigabit101@gmail.com>
2014-09-20 18:32:49 +0100

    Fixed Recipes Not Loading

--- src/main/java/Talismans2/recipe/CraftingHandler.java
+++ src/main/java/Talismans2/recipe/CraftingHandler.java
@@ -9,33 +9,38 @@ public class CraftingHandler {
 
     public static void CopyTalismans(ConfigTalismans properties) {
         if (properties.CopyTalismansTrue)
-            GameRegistry.addShapelessRecipe(new ItemStack(ModItems.FlameTalisman, 2),
-                    new Object[]{new ItemStack(ModItems.BlankTalisman),
-                            ModItems.FlameTalisman});
+        	addCopyRecipes();
+ 
+    }
+    
+    public static void addCopyRecipes(){
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.FlameTalisman, 2),
+    		new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.FlameTalisman});
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.CraftingTalisman, 2),
-                new Object[]{new ItemStack(ModItems.BlankTalisman),
-                        ModItems.CraftingTalisman});
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.CraftingTalisman, 2),
+            new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.CraftingTalisman});
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.InvisibleTalisman, 2),
-                new Object[]{new ItemStack(ModItems.BlankTalisman),
-                        ModItems.InvisibleTalisman});
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.InvisibleTalisman, 2),
+            new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.InvisibleTalisman});
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.LightTalisman, 2),
-                new Object[]{new ItemStack(ModItems.BlankTalisman),
-                        ModItems.LightTalisman});
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.LightTalisman, 2),
+            new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.LightTalisman});
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.MiningTalisman, 2),
-                new Object[]{new ItemStack(ModItems.BlankTalisman),
-                        ModItems.MiningTalisman});
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.MiningTalisman, 2),
+            new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.MiningTalisman});
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.MovementTalisman, 2),
-                new Object[]{new ItemStack(ModItems.BlankTalisman),
-                        ModItems.MovementTalisman});
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.MovementTalisman, 2),
+            new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.MovementTalisman});
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModItems.WitherlessTalisman, 2),
-                new Object[]{new ItemStack(ModItems.BlankTalisman),
-                        ModItems.WitherlessTalisman});
-
+    GameRegistry.addShapelessRecipe(new ItemStack(ModItems.WitherlessTalisman, 2),
+            new Object[]{new ItemStack(ModItems.BlankTalisman),
+                    ModItems.WitherlessTalisman});
+    	
     }
 }
