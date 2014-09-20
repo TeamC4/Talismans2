3000e0b7cc9a0d300b9d164a3a4d653077b50886
Gig <thegigabit101@gmail.com>
2014-09-20 18:28:28 +0100

    Clean Added More Blood Alter Recipes

--- src/main/java/Talismans2/module/bloodmagic/BloodMagicRecipes.java
+++ src/main/java/Talismans2/module/bloodmagic/BloodMagicRecipes.java
@@ -3,6 +3,7 @@ package Talismans2.module.bloodmagic;
 import Talismans2.util.TalismanStacks;
 import WayofTime.alchemicalWizardry.api.altarRecipeRegistry.AltarRecipeRegistry;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 
 public class BloodMagicRecipes {
@@ -11,6 +12,14 @@ public class BloodMagicRecipes {
     public static void init() {
         //Altar Recipes
         AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanCrafting, new ItemStack(Blocks.crafting_table), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanFlame, new ItemStack(Items.flint_and_steel), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanInvisible, new ItemStack(Items.carrot), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanLight, new ItemStack(Blocks.torch), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanMining, new ItemStack(Items.diamond_pickaxe), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanMovement, new ItemStack(Items.diamond_boots), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanWater, new ItemStack(Items.water_bucket), 3, 6000, 10, 1, false);
+        AltarRecipeRegistry.registerAltarRecipe(TalismanStacks.talismanWitherless, new ItemStack(Blocks.skull), 3, 6000, 10, 1, false);
+
 
     }
 }
