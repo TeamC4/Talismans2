5206c7dac577b0ba5586a51332b1382c565d6e67
Gig <thegigabit101@gmail.com>
2014-10-05 18:58:22 +0100

    Added New Magnet Talisman

--- src/main/java/Talismans2/init/ModItems.java
+++ src/main/java/Talismans2/init/ModItems.java
@@ -1,5 +1,6 @@
 package Talismans2.init;
 
+import vazkii.botania.common.item.equipment.bauble.ItemMagnetRing;
 import net.minecraft.item.Item;
 import Talismans2.item.misc.ItemTalismanRings;
 import Talismans2.item.talismans.ItemBlankTalisman;
@@ -7,6 +8,7 @@ import Talismans2.item.talismans.ItemCraftingTalisman;
 import Talismans2.item.talismans.ItemFlameTalisman;
 import Talismans2.item.talismans.ItemInvisibleTalisman;
 import Talismans2.item.talismans.ItemLightTalisman;
+import Talismans2.item.talismans.ItemMagnetTalisman;
 import Talismans2.item.talismans.ItemMiningTalisman;
 import Talismans2.item.talismans.ItemMovementTalisman;
 import Talismans2.item.talismans.ItemWaterTalisman;
@@ -29,6 +31,7 @@ public class ModItems {
 	public static Item InvisibleTalisman;
 	public static Item WitherlessTalisman;
 	public static Item CraftingTalisman;
+	public static Item MagnetTalisman;
 	public static Item TalismanRings;
 	public static Item TalismansDubugTool;
 
@@ -60,6 +63,9 @@ public class ModItems {
 		// Crafting Talisman
 		CraftingTalisman = new ItemCraftingTalisman();
 		GameRegistry.registerItem(CraftingTalisman, "Crafting Talisman");
+		// Magnet Talisman
+		MagnetTalisman = new ItemMagnetTalisman();
+		GameRegistry.registerItem(MagnetTalisman, "Magnet Talisman");
 		// Talisman Rings
 		TalismanRings = new ItemTalismanRings();
 		GameRegistry.registerItem(TalismanRings, "Talisman Rings");
