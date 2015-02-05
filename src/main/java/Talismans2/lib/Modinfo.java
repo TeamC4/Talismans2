62ef09a4d7305471748aeaeb19efdf0d3985d787
Gig <thegigabit101@gmail.com>
2015-02-05 14:26:22 +0000

    Version 2.1.1

--- src/main/java/Talismans2/lib/Modinfo.java
+++ src/main/java/Talismans2/lib/Modinfo.java
@@ -11,7 +11,7 @@ public class Modinfo {
 	// Mod Name
 	public static final String NAME = "Talismans 2";
 	// Mod Version
-	public static final String Version = "2.1.0";
+	public static final String Version = "2.1.1";
 	// Common Proxy
 	public static final String SERVER_PROXY_CLASS = "Talismans2.proxies.CommonProxy";
 	// Client Proxy
